534316825f26236c10dc797a212f046b57fd8547
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2025-08-07 21:49:56 +0100

    implement music cog info command

--- icebeat/cogs/music.py
+++ icebeat/cogs/music.py
@@ -360,33 +360,6 @@ class Music(commands.Cog):
         guild_only=True,
     )
 
-    # @_channel_group.command(
-    #     name="status", description="current state of exclusive text channel mode"
-    # )
-    # @_is_whitelisted()
-    # @_is_guild_owner()
-    # @_bot_has_permissions()
-    # @_cooldown()
-    # async def channel_status(self, interaction: Interaction) -> None:
-    #     guild_id: int = interaction.guild_id  # pyright: ignore[reportAssignmentType]
-    #
-    #     guild = await self._bot.store.get_guild(guild_id)  # pyright: ignore[reportArgumentType]
-    #
-    #     state = "enabled" if guild.text_channel else "disabled"
-    #     text_channel = "not set"
-    #     if guild.text_channel_id:
-    #         if not interaction.guild.get_channel(guild.text_channel_id):  # pyright: ignore[reportOptionalMemberAccess]
-    #             await self._bot.store.unset_guild_text_channel_id(guild_id)
-    #         else:
-    #             text_channel = f"<#{guild.text_channel_id}>"
-    #     embed = Embed(
-    #         title="Exclusive Text Channel State:",
-    #         color=Color.green(),
-    #     )
-    #     embed.add_field(name="State", value=f"_{state}_")
-    #     embed.add_field(name="Text Channel", value=text_channel)
-    #     await interaction.response.send_message(embed=embed, ephemeral=True)
-
     @_channel_group.command(
         name="enable", description="enable exclusive text channel mode, if set"
     )
@@ -472,8 +445,29 @@ class Music(commands.Cog):
     @_bot_has_permissions()
     @_cooldown()
     async def info(self, interaction: Interaction) -> None:
-        # send ephemeral message
-        pass  # TODO: implement
+        guild_id: int = interaction.guild_id  # pyright: ignore[reportAssignmentType]
+
+        guild = await self._bot.store.get_guild(guild_id)  # pyright: ignore[reportArgumentType]
+
+        embed = Embed(
+            title="Player Info:",
+            color=Color.green(),
+        )
+        state = "enabled" if guild.text_channel else "disabled"
+        text_channel = "not set"
+        if guild.text_channel_id:
+            if not interaction.guild.get_channel(guild.text_channel_id):  # pyright: ignore[reportOptionalMemberAccess]
+                await self._bot.store.unset_guild_text_channel_id(guild_id)
+            else:
+                text_channel = f"<#{guild.text_channel_id}>"
+        presence = "leave" if guild.auto_leave else "stay"
+        embed.description = (
+            f"**Text Channel:** {state}, {text_channel}\n"
+            f"**Filter:** {guild.filter.name}\n"
+            f"**Volume:** {guild.volume}\n"
+            f"**When Queue Ends:** {presence}"
+        )
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
     async def cog_app_command_error(
         self, interaction: Interaction, error: app_commands.AppCommandError
